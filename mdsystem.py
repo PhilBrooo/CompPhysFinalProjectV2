ee7eb85475aeb484cda6ade04fcbcd7c91223cb1
PhilBrooo <45039878+PhilBrooo@users.noreply.github.com>
2021-09-16 15:49:11 +0200

    Uploaded MDsystem file to get started

---
+++ mdsystem.py
@@ -0,0 +1,17 @@
+import numpy as np
+#Simulation system of hard sphere liquid in 3D. 
+#
+#
+#
+#
+#
+class MDsystem(object):
+    
+    def __init__(self):
+        resetMDsys()
+        
+        
+        
+        
+    def resetMDsys():
+        self.t = 0
